52844f79034afa80dbc88e77d20d57bc3f19123f
Luis Rosales <luismrosales2@gmail.com>
2022-07-08 23:13:51 -0400

    bug fixes and mobile view

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ function animateSlides() {
     slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
     slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
     slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
-    slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "0.5");
     // create a scene
     slideScene = new ScrollMagic.Scene({
       triggerElement: slide,
@@ -125,12 +124,6 @@ barba.init({
       beforeEnter() {
         logo.href = "../index.html";
         detailAnimation();
-        gsap.fromTo(
-          ".nav-header",
-          1,
-          { y: "100%" },
-          { y: "0%", ease: "power2.inOut" }
-        );
       },
       beforeLeave() {
         controller.destroy();
@@ -163,9 +156,17 @@ barba.init({
           ".swipe",
           1,
           { x: "0%" },
+
           { x: "100%", stagger: 0.25, onComplete: done }
         );
         tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+        tl.fromTo(
+          ".nav-header",
+          1,
+          { y: "-100%" },
+          { y: "0%", ease: "power2.inOut" },
+          "-=1.5"
+        );
       },
     },
   ],
